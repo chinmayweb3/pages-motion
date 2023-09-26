fcdcc68ac21553c717134c34f2c89f54094757a8
chinmayweb3 <chinmayweb3@gmail.com>
2023-09-26 23:07:28 +0530

    home left side

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { framerComponent } from "../components/HOC/framerComponent";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -10,10 +10,13 @@ const Home = () => {
 
       <div className="absolute w-[90%] h-[100px] flex-center">
         <div className="basis-[50%] h-screen flex justify-center flex-col">
-          <p className="text-orange-500 font-orbitron font-semibold tracking-[2px] text-[16px]">UI Developer</p>
+          <p className="text-[#f4cd58] font-orbitron font-semibold tracking-[2px] text-[16px]">UI Developer</p>
           <h1 className="text-[72px] font-rajdhani font-black text-offBlack leading-[1]">
             Lorem Lipsum <br /> Lorem Lipsu
           </h1>
+          <button className="w-fit px-[25px] text-[18px] whitespace-nowrap font-bold flex-center gap-[10px] font-rajdhani py-[10px] mt-[20px] shadow-[0_20px_45px_#00000030] duration-500 hover:shadow-[0_0_45px_#00000026] ">
+            HIRE ME ! <AiOutlineArrowRight strokeWidth={35} fill="#111115" />
+          </button>
         </div>
         <div className="basis-[50%] h-screen "></div>
       </div>
