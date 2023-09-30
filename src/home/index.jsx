1eda91a11538682bbb88416b39c687275822d04a
chinmayweb3 <chinmayweb3@gmail.com>
2023-10-01 01:08:09 +0530

    / added clipPath

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -8,6 +8,21 @@ const Home = () => {
     <main className="h-[100vh] flex-center">
       <div className="basis-[50%] h-screen bg-offWhite dark:bg-offBlack d-mode"></div>
       <div className="basis-[50%] h-screen bg-offBlack dark:bg-offWhite d-mode "></div>
+      <div className="absolute top-0 left-0 w-full h-full flex">
+        <div
+          style={{
+            clipPath: "polygon(20% 38%, 0 0, 0 54%, 47% 26%, 36% 59%, 95% 81%, 100% 59%, 100% 100%, 63% 48%, 57% 93%, 19% 50%, 77% 28%)",
+          }}
+          className="dark:opacity-0 opacity-30 w-[50%] h-full bg-gradient-to-tr from-yellow-600 dark:from-cyan-700 to-cyan-600 dark:to-rose-500"
+        />
+        <div
+          style={{
+            transform: "rotateY(180deg)",
+            clipPath: "polygon(20% 38%, 0 0, 0 54%, 47% 26%, 36% 59%, 95% 81%, 100% 59%, 100% 100%, 63% 48%, 57% 93%, 19% 50%, 77% 28%)",
+          }}
+          className="opacity-0 dark:opacity-30 w-[50%] h-full bg-gradient-to-tr from-yellow-600 dark:from-pink-600 to-cyan-600 dark:to-rose-500"
+        />
+      </div>
 
       <div className="absolute w-[90%] h-[100px] flex-center">
         <LeftSection />
